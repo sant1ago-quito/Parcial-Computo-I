bbc24538b5632774821362c217755ccd6530468e
GabyVasquez07 <gabrielaestefanivasquez@gmail.com>
2024-08-30 08:18:31 -0600

    Cambios

---
+++ Ejercicio3.py
@@ -0,0 +1,14 @@
+"""
+Un hotel de playa cuenta con un recepcionista que se encarga de
+presentar a los clientes las opciones de habitaciones disponibles junto
+con sus precios. Tras la elección de la habitación, el recepcionista
+solicita los datos personales del cliente y el número de noches que
+permanecerá en el hotel. Finalmente, entrega al cliente una factura
+detallada con el total de los gastos.
+ Adicionalmente, los clientes pueden solicitar servicios extra,
+como el uso de la piscina o la cancha de golf, que tienen un costo
+adicional. Implementa esta funcionalidad en tu programa
+"""
+class Hotel():
+    def __init__(self):
+        pass
